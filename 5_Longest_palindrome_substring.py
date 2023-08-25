9dcd85bf3af6e14fccf38a6d0a3b5e0d90858be7
Thomas Oakes <toakes01@manhattan.edu>
2023-08-25 09:18:27 -0400

    Added 5_Longest_palindrome_substring solution code

---
+++ 5_Longest_palindrome_substring.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        start = 0 
+        end = 0
+
+        for i in range(len(s)):
+            len1 = expandAroundCenter(s, i, i)
+            len2 = expandAroundCenter(s, i, i+1)
+
+            length = max(len1, len2)
+
+            if length > end - start:
+                start = i - (length - 1) // 2
+                end = i + length // 2
+
+        return s[start:end+1]
+
+
+def expandAroundCenter(s, left, right):
+    while left>=0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+
+    return right - left - 1
