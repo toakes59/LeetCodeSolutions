0b771dacce0e36561f534893183bbb1296765d9b
Thomas Oakes <toakes01@manhattan.edu>
2023-08-30 20:13:54 -0400

    Added 9_Palindrome_number solution code

---
+++ 9_Palindrome_number.py
@@ -0,0 +1,14 @@
+# Palindrome_number
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        reversed_num = 0
+        temp = x
+
+        while temp > 0:
+            reversed_num = reversed_num * 10 + temp % 10
+            temp //= 10
+
+        return x == reversed_num
